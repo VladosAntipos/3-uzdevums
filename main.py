81e4b78da118496372988335a649d00d3934e3aa
VladosAntipos <95612383+VladosAntipos@users.noreply.github.com>
2023-03-06 07:32:02 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(1,11)
+y = x
+
+
+plt.title("Line graph")
+plt.xlabel("X axis")
+plt.ylabel("Y axis")
+plt.plot(x, y, color ="red")
+plt.show()
